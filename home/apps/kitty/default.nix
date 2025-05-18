fe4face5b74aea48ad7fbe1f7d82a68e92832685
avivbintangaringga <avivbintangaringga90@gmail.com>
2025-05-18 11:34:39 +0700

    before update

--- home/apps/kitty/default.nix
+++ home/apps/kitty/default.nix
@@ -7,6 +7,8 @@
     settings = {
       background_opacity = 0.8;
       confirm_os_window_close = 0;
+      #window_padding_width = 4;
+      enable_audio_bell = "no";
     };
     #themeFile = "Catppuccin-Mocha";
   };
