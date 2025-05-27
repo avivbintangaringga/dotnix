18b169a5cbece5d77e97abb63d3f9d73c0658340
avivbintangaringga <avivbintangaringga90@gmail.com>
2025-05-27 14:47:21 +0700

    add lutris

--- modules/home/apps/gaming/default.nix
+++ modules/home/apps/gaming/default.nix
@@ -7,6 +7,7 @@
   config = lib.mkIf config.setup.apps.gaming.all.enable {
     setup.apps.gaming = {
       heroic.enable = true;
+      lutris.enable = true;
       mangohud.enable = true;
       protonup-qt.enable = true;
       wine.enable = true;
