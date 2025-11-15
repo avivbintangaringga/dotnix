b422d4f6cdcd8baadbf7064c8166a094a6c098f9
avivbintangaringga <avivbintangaringga90@gmail.com>
2025-11-15 10:23:23 +0700

    add nix-ld

--- modules/nixos/misc/nix-ld/default.nix
+++ modules/nixos/misc/nix-ld/default.nix
@@ -16,7 +16,31 @@
       enable = true;
       libraries = options.programs.nix-ld.libraries.default ++ (with pkgs; [
         glib
-        glibc
+        nss
+        nspr
+        dbus
+        atk
+        cups
+        gtk3
+        pango
+        cairo
+        xorg.libX11
+        xorg.libXcomposite
+        xorg.libXdamage
+        xorg.libXext
+        xorg.libXfixes
+        xorg.libXrandr
+        xorg.libxcb
+        libdrm
+        mesa
+        alsa-lib
+        libGL
+        udev
+        libgbm
+        expat
+        libxkbcommon
+        gnome.gvfs
+        harfbuzzFull
       ]);
     };
   };
