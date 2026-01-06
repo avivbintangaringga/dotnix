852c9ebf12f2dc9d64049d49a8aa105f2fe9fd8b
avivbintangaringga <avivbintangaringga90@gmail.com>
2026-01-06 19:31:01 +0700

    change mime handler

--- users/r7fx.nix
+++ users/r7fx.nix
@@ -112,7 +112,19 @@ in
         let
           archives = [
             "application/zip"
-            "application/vnd.rar"
+            "application/x-tar"
+            "application/x-gzip"
+            "application/x-bzip2"
+            "application/x-xz"
+            "application/x-7z-compressed"
+            "application/x-rar-compressed"
+            "application/x-rar"
+            "application/x-cpio"
+            "application/x-iso9660-image"
+            "application/x-debian-package"
+            "application/vnd.android.package-archive"
+            "application/x-redhat-package-manager"
+            "application/x-zstd"
           ];
           browser = [
             "text/html"
@@ -121,14 +133,57 @@ in
             "x-scheme-handler/about"
             "x-scheme-handler/unknown"
           ];
+          images = [
+            "image/jpeg"
+            "image/png"
+            "image/gif"
+            "image/webp"
+            "image/avif"
+            "image/heic"
+            "image/heif"
+            "image/svg+xml"
+            "image/bmp"
+          ];
+          videos = [
+            "video/mp4"
+            "video/webm"
+            "video/x-matroska"
+            "video/quicktime"
+            "video/x-msvideo"
+            "video/mpeg"
+            "video/ogg"
+            "video/mp2t"
+            "video/3gpp"
+          ];
+          audios = [
+            "audio/mpeg"
+            "audio/mp4"
+            "audio/ogg"
+            "audio/wav"
+            "audio/x-wav"
+            "audio/flac"
+            "audio/x-flac"
+            "audio/aac"
+            "audio/webm"
+            "audio/x-matroska"
+            "audio/midi"
+            "audio/x-midi"
+            "audio/opus"
+          ];
         in
         lib.mkMerge [
           {
             "inode/directory" = [ "org.gnome.Nautilus.desktop" ];
             "x-scheme-handler/discord" = [ "discord.desktop" ];
+            "x-scheme-handler/terminal" = [ "kitty.desktop" ];
+            "application/x-*script" = [ "org.gnome.gedit.desktop" ];
+            "text/plain" = [ "org.gnome.gedit.desktop" ];
           }
-          (mylib.listToAttrsSameValue browser [ "zen-beta.desktop" ])
+          (mylib.listToAttrsSameValue browser [ "zen-beta.desktop" "thorium-browser.desktop" ])
           (mylib.listToAttrsSameValue archives [ "peazip.desktop" ])
+          (mylib.listToAttrsSameValue images [ "org.kde.gwenview.desktop" "gimp.desktop" ])
+          (mylib.listToAttrsSameValue videos [ "vlc.desktop" ])
+          (mylib.listToAttrsSameValue audios [ "vlc.desktop" ])
         ];
     };
 
