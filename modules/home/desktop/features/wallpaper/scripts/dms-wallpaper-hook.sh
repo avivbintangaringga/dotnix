b1399158a956901b05c91ae47137601b1ef01773
avivbintangaringga <avivbintangaringga90@gmail.com>
2026-02-15 21:28:04 +0700

    change dms wallpaper hook aura command

--- modules/home/desktop/features/wallpaper/scripts/dms-wallpaper-hook.sh
+++ modules/home/desktop/features/wallpaper/scripts/dms-wallpaper-hook.sh
@@ -3,4 +3,4 @@
 sleep 2
 
 primary="$(jq -r ".colors.dark.primary" $HOME/.cache/DankMaterialShell/dms-colors.json | cut -d "#" -f2-)"
-asusctl aura static -c $primary
+asusctl aura effect static --colour $primary
