512a16379009f920fd31e7e2264f23b949498b63
avivbintangaringga <avivbintangaringga90@gmail.com>
2026-02-17 22:56:28 +0700

    add kdeconnect

---
+++ modules/home/services/kdeconnect/default.nix
@@ -0,0 +1,12 @@
+{ lib, config, ... }:
+{
+  options = {
+    setup.services.kdeconnect.enable = lib.mkEnableOption "KDE Connect";
+  };
+
+  config = lib.mkIf config.setup.services.kdeconnect.enable {
+    services.kdeconnect = {
+      enable = true;
+    };
+  };
+}
--- users/r7fx.nix
+++ users/r7fx.nix
@@ -78,6 +78,7 @@ in
     services = {
       blueman = enabled;
       cliphist = enabled;
+      kdeconnect = enabled;
       playerctld = enabled;
     };
   };
