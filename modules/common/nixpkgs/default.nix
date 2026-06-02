ea86d0a5a243ebf4e8aaee3f9cd2fe80e957fc04
avivbintangaringga <avivbintangaringga90@gmail.com>
2026-06-02 15:20:07 +0700

    upgrade

--- modules/common/nixpkgs/default.nix
+++ modules/common/nixpkgs/default.nix
@@ -6,6 +6,9 @@
       };
       allowUnfree = true;
       allowUnfreePredicate = (_: true);
+      permittedInsecurePackages = [
+        "electron-39.8.10"
+      ];
       # cudaSupport = true;
     };
   };
