b9625f4319cfe59a681b2b172ccdfd4d749a67d7
avivbintangaringga <avivbintangaringga90@gmail.com>
2026-06-03 13:53:43 +0700

    nvidia gpu power fix

--- modules/nixos/hardware/nvidia/default.nix
+++ modules/nixos/hardware/nvidia/default.nix
@@ -16,13 +16,14 @@
       nvidia-container-toolkit
     ];
 
-    systemd = {
-      services = {
-        "nvidia-powerd" = {
-          wantedBy = lib.mkForce [];
-        };
-      };
-    };
+    # systemd = {
+    #   services = {
+    #     "nvidia-powerd" = {
+    #       enable = false;
+    #       wantedBy = lib.mkForce [];
+    #     };
+    #   };
+    # };
 
     # environment.variables = {
     #   __EGL_VENDOR_LIBRARY_FILENAMES = "/run/opengl-driver/share/glvnd/egl_vendor.d/50_mesa.json";
@@ -40,7 +41,19 @@
           nvidiaBusId = lib.mkForce "PCI:1:0:0";
           amdgpuBusId = lib.mkForce "PCI:6:0:0";
         };
+        powerManagement = {
+          enable = true;
+          finegrained = false;
+        };
+        dynamicBoost = {
+          enable = true;
+        };
+        nvidiaPersistenced = true;
       };
     };
+
+    boot.extraModprobeConfig = ''
+      options nvidia NVreg_RegistryDwords="PowerMizerEnable=0x1; PerfLevelSrc=0x2222; PowerMizerDefaultAC=0x1"
+    '';
   };
 }
