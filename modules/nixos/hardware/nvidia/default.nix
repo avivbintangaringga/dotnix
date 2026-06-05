3d9b7374d8f87ee07df591a9438513596de37539
avivbintangaringga <avivbintangaringga90@gmail.com>
2026-06-05 17:48:13 +0700

    change nvidia hardware settings

--- modules/nixos/hardware/nvidia/default.nix
+++ modules/nixos/hardware/nvidia/default.nix
@@ -16,49 +16,21 @@
       nvidia-container-toolkit
     ];
 
-    # systemd = {
-    #   services = {
-    #     "nvidia-powerd" = {
-    #       enable = false;
-    #       wantedBy = lib.mkForce [];
-    #     };
-    #   };
-    # };
-
-    # environment.variables = {
-    #   __EGL_VENDOR_LIBRARY_FILENAMES = "/run/opengl-driver/share/glvnd/egl_vendor.d/50_mesa.json";
-    # };
-
     hardware = {
       graphics.enable = true;
+
       nvidia-container-toolkit = {
         enable = true;
       };
+
       nvidia = {
-        modesetting.enable = true;
-        open = false;
-        prime = {
-          offload = {
-            enable = true;
-            enableOffloadCmd = true;
-          };
-          nvidiaBusId = lib.mkForce "PCI:1:0:0";
-          amdgpuBusId = lib.mkForce "PCI:6:0:0";
-        };
         powerManagement = {
           enable = true;
           finegrained = false;
         };
-        dynamicBoost = {
-          enable = false;
-        };
+
         nvidiaPersistenced = true;
       };
     };
-
-    boot.extraModprobeConfig = ''
-      options nvidia NVreg_RegistryDwords="PowerMizerEnable=0x1; PerfLevelSrc=0x2222; PowerMizerLevel=0x1; PowerMizerDefaultAC=0x1"
-      options nvidia_drm modeset=1
-    '';
   };
 }
