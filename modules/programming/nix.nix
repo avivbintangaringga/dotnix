e87f0cc6384d7f661b9b4668b0f9187dd34fb643
avivbintangaringga <avivbintangaringga90@gmail.com>
2026-07-27 13:42:48 +0700

    adjust pedantix settings

--- modules/programming/nix.nix
+++ modules/programming/nix.nix
@@ -20,9 +20,21 @@
       programs.pedantix = {
         enable = true;
         settings = {
+          top-level-blank-lines = 1;
+          overrides = [
+            {
+              attrs.first = [ "url" ];
+              path = "inputs";
+            }
+            {
+              attrs.blank-lines = 1;
+              path = "flake-file.inputs";
+            }
+          ];
           args = {
             first = [
               "self"
+              "name"
               "den"
               "dotnix"
               "inputs"
@@ -36,10 +48,10 @@
               "myLib"
               "pkgs"
               "options"
-              "modulesPath"
               "utils"
             ];
             last = [
+              "modulesPath"
               "<defaulted>"
               "..."
             ];
@@ -48,26 +60,43 @@
             first = [
               "url"
               "inputs"
+              "flake-file"
+              "flake"
+
+              "includes"
+              "homeManager"
+              "nixos"
+              "imports"
 
               "options"
               "config"
               "enable"
               "package"
 
-              "flake-file"
-              "includes"
-
-              "homeManager"
-              "nixos"
-              "imports"
               "home"
               "environment"
 
+              "programs"
+              "services"
             ];
             last = [ "meta" ];
+            flatten = true;
             merge = true;
           };
-          preset = "nixos-module";
+          inherits = {
+            sort = true;
+            first = [
+              "self"
+              "name"
+              "homeConfig"
+              "inputs"
+              "config"
+              "lib"
+              "pkgs"
+              "options"
+              "modulesPath"
+            ];
+          };
         };
       };
     };
