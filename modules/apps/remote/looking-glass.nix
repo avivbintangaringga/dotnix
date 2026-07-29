88f437251eabdd7fff3952e3ad9d2d3126c6e3c9
Aviv Bintang Aringga <avivbintangaringga90@gmail.com>
2026-07-30 06:49:24 +0700

    custom looking glass version

--- modules/apps/remote/looking-glass.nix
+++ modules/apps/remote/looking-glass.nix
@@ -3,11 +3,31 @@
     programs.looking-glass-client = {
       enable = true;
       package = pkgs.looking-glass-client.override {
-        pipewireSupport = false;
+        # pipewireSupport = false;
       };
       settings = {
 
       };
     };
+    nixpkgs.overlays = [
+      (_final: prev: {
+        looking-glass-client = prev.looking-glass-client.overrideAttrs (oldAttrs: {
+          buildInputs =
+            (oldAttrs.buildInputs or [ ])
+            ++ (with pkgs; [
+              libunwind
+              elfutils
+            ]);
+          src = prev.fetchFromGitHub {
+            fetchSubmodules = true;
+            hash = "sha256-IDNxJrMMMvZk/PTKcPXXLrFRrLujwE/apfwHfLPjh1k=";
+            owner = "gnif";
+            repo = "LookingGlass";
+            rev = "5196c5503ae87cc4f0decd17aa556973fc4a7308";
+          };
+          version = "5196c550";
+        });
+      })
+    ];
   };
 }
