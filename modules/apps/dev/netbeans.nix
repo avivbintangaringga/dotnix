7ee9524a6f2fe8be370302468205f211e02edc62
Aviv Bintang Aringga <avivbintangaringga90@gmail.com>
2026-08-05 21:59:21 +0700

    add netbeans env vars

--- modules/apps/dev/netbeans.nix
+++ modules/apps/dev/netbeans.nix
@@ -1,10 +1,14 @@
 {
-  dotnix.netbeans = {
-    homeManager = { pkgs, ... }: {
-      home.packages = with pkgs; [
+  dotnix.netbeans.homeManager = { pkgs, ... }: {
+    home = {
+      packages = with pkgs; [
         jre
         netbeans
       ];
+      sessionVariables = {
+        AWT_TOOLKIT = "MToolkit";
+        _JAVA_AWT_WM_NONREPARENTING = 1;
+      };
     };
   };
 }
