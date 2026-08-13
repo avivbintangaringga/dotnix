cbef92b772bc961d65150f53a4269a9620ba014a
Aviv Bintang Aringga <avivbintangaringga90@gmail.com>
2026-08-13 23:19:40 +0700

    change docker config

--- modules/virtualization/docker.nix
+++ modules/virtualization/docker.nix
@@ -1,11 +1,18 @@
 {
   dotnix.docker = { user, ... }: {
     nixos = {
+      networking.firewall.trustedInterfaces = [ "docker0" ];
       users.users.${user.userName}.extraGroups = [ "docker" ];
       virtualisation.docker = {
         enable = true;
-        enableOnBoot = false;
-        rootless.enable = true;
+        enableOnBoot = true;
+        daemon.settings = {
+          dns = [ "1.1.1.1" "8.8.8.8" ];
+        };
+        rootless = {
+          enable = true;
+          setSocketVariable = true;
+        };
       };
     };
   };
