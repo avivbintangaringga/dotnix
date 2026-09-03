06d8b719261504f536b1c55dc56bbaa0facacb50
Aviv Bintang Aringga <avivbintangaringga90@gmail.com>
2026-09-03 10:03:40 +0700

    change umbriel config

--- modules/desktop/wm/umbriel.nix
+++ modules/desktop/wm/umbriel.nix
@@ -5,26 +5,14 @@
 }:
 {
   dotnix.umbriel = {
-    includes = with dotnix; [
-      noctalia
-      vicinae
-    ];
-    
     homeManager = { pkgs, ... }: {
       imports = [
         inputs.umbriel.homeModules.default
       ];
-
       nixpkgs.overlays = [
         inputs.umbriel.overlays.default
         inputs.xdg-desktop-portal-umbriel.overlays.default
       ];
-
-      xdg.portal.extraPortals = with pkgs; [
-        xdg-desktop-portal-umbriel
-        xdg-desktop-portal-gtk
-      ];
-
       programs.umbriel = {
         enable = true;
         settings = {
@@ -32,17 +20,17 @@
             curve = "easeout";
             duration_ms = 200;
             enabled = true;
+            scratchpad = {
+              blur = true;
+              enabled = true;
+            };
             windows_in = {
-              style = "zoom";
               duration_ms = 130;
+              style = "zoom";
             };
             windows_out = {
-              style = "slide";
               duration_ms = 130;
-            };
-            scratchpad = {
-              enabled = true;
-              blur = true;
+              style = "slide";
             };
           };
           appearance = {
@@ -105,8 +93,12 @@
             "Mod+Ctrl+L" = "window-move-or-output-right";
             "Mod+Ctrl+Left" = "window-move-or-output-left";
             "Mod+Ctrl+Right" = "window-move-or-output-right";
+            "Mod+Ctrl+Shift+WheelDown" = "window-move-or-output-right";
+            "Mod+Ctrl+Shift+WheelUp" = "window-move-or-output-left";
             "Mod+Ctrl+Up" = "window-move-or-workspace-up";
             "Mod+Ctrl+V" = "window-toggle-floating";
+            "Mod+Ctrl+WheelDown" = "window-move-or-workspace-down";
+            "Mod+Ctrl+WheelUp" = "window-move-or-workspace-up";
             "Mod+Down" = "window-focus-or-workspace-down";
             "Mod+E" = "spawn:nautilus";
             "Mod+F" = "window-toggle-maximize";
@@ -123,15 +115,19 @@
             "Mod+Return" = "spawn:kitty";
             "Mod+Right" = "window-focus-or-output-right";
             "Mod+S" = "scratchpad-toggle";
-            "Mod+Shift+S" = "window-toggle-scratchpad";
             "Mod+Shift+F" = "window-toggle-fullscreen";
             "Mod+Shift+R" = "window-cycle-height";
+            "Mod+Shift+S" = "window-toggle-scratchpad";
             "Mod+Shift+V" = "window-focus-switch-floating";
+            "Mod+Shift+WheelDown" = "window-focus-or-output-right";
+            "Mod+Shift+WheelUp" = "window-focus-or-output-left";
             # TODO: AUDIO binds
             "Mod+Space" = "spawn:vicinae toggle";
             "Mod+Tab" = "scratchpad-focus-next";
             "Mod+Up" = "window-focus-or-workspace-up";
             "Mod+V" = "spawn:vicinae deeplink vicinae://launch/clipboard/history";
+            "Mod+WheelDown" = "window-focus-or-workspace-down";
+            "Mod+WheelUp" = "window-focus-or-workspace-up";
             "Mod+Z" = "spawn:zeditor";
             # TODO: Screenshot
           };
@@ -195,16 +191,16 @@
             }
             {
               default_floating = true;
-              default_pinned = true;
+              default_height = 0.2;
               default_maximize = false;
               default_maximize_to_edges = false;
-              default_width = 0.2;
-              default_height = 0.2;
+              default_pinned = true;
               default_position = {
                 anchor = "bottom_right";
                 x = 30;
                 y = 30;
               };
+              default_width = 0.2;
               match.title = "^(Picture-in-Picture|Picture in picture)$";
             }
             {
@@ -222,8 +218,15 @@
           };
         };
       };
+      xdg.portal.extraPortals = with pkgs; [
+        xdg-desktop-portal-umbriel
+        xdg-desktop-portal-gtk
+      ];
     };
-
+    includes = with dotnix; [
+      noctalia
+      vicinae
+    ];
     nixos = { pkgs, ... }: {
       environment.systemPackages = with pkgs; [
         xdg-desktop-portal-umbriel
