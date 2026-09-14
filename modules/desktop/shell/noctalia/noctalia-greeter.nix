b8535ad4d84746d3e50066bb6cef128c97780877
avivbintangaringga <avivbintangaringga90@gmail.com>
2026-09-14 20:20:22 +0700

    change noctalia-greeter program to service

--- modules/desktop/shell/noctalia/noctalia-greeter.nix
+++ modules/desktop/shell/noctalia/noctalia-greeter.nix
@@ -3,38 +3,34 @@
   ...
 }:
 {
-  flake-file.inputs.noctalia-greeter = {
-    url = "github:noctalia-dev/noctalia-greeter";
-    inputs.nixpkgs.follows = "nixpkgs";
-  };
-
   dotnix.noctalia-greeter = { user, ... }: {
     nixos = {
       imports = [
         inputs.noctalia-greeter.nixosModules.default
       ];
-      programs = {
-        noctalia-greeter = {
-          enable = true;
-          greeter-args = "--session start-umbriel";
-          settings = {
-            appearance.hide_logo = true;
-            cursor = {
-              size = 24;
-              theme = "Bibata-Modern-Classic";
-            };
-            output.scale = 1.0;
-            session.default = "Umbriel";
-            user.default = user.userName;
-          };
-        };
-
-        seahorse.enable = true;
-      };
       services = {
-        displayManager.autoLogin = {
-          enable = true;
-          user = user.userName;
+        displayManager = {
+          autoLogin = {
+            enable = true;
+            user = user.userName;
+          };
+          noctalia-greeter = {
+            enable = true;
+            greeter-args = "--session start-umbriel";
+            passwordless-sync-users = [
+              "${user.userName}"
+            ];
+            settings = {
+              appearance.hide_logo = true;
+              cursor = {
+                size = 24;
+                theme = "Bibata-Modern-Classic";
+              };
+              output.scale = 1.0;
+              session.default = "Umbriel";
+              user.default = user.userName;
+            };
+          };
         };
         greetd = {
           enable = true;
@@ -44,7 +40,10 @@
           };
         };
       };
-      users.users.${user.userName}.extraGroups = [ "greeter" ];
     };
+  };
+  flake-file.inputs.noctalia-greeter = {
+    inputs.nixpkgs.follows = "nixpkgs";
+    url = "github:noctalia-dev/noctalia-greeter";
   };
 }
