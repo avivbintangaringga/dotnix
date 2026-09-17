9c257de05874d71780f8404fbe73b23bc2d3e631
avivbintangaringga <avivbintangaringga90@gmail.com>
2026-09-17 19:04:09 +0700

    fix upgrade command

--- modules/shell/zsh.nix
+++ modules/shell/zsh.nix
@@ -50,7 +50,9 @@
 
                 git config user.email ${home.git.email}
                 git config user.name ${home.git.userName}
-                echo "INFO: Using github user ${home.git.userName} with auth token ending in ...$(echo \"$TOKEN\" | rev | cut -c 4- | rev)"
+
+                TOKEN_CUT=$(echo "$TOKEN" | rev | cut -c -4 | rev)
+                echo "INFO: Using github user ${home.git.userName} with auth token ending in ...$TOKEN_CUT"
 
                 $SUDO nh os switch . --no-nom --show-trace --update $NH_ASK $NH_SUDO $NH_COMMIT -- --option access-tokens "github.com=$TOKEN"
                 nh home switch . --no-nom -b hm-bak --show-trace --update $NH_ASK $NH_COMMIT -- --option access-tokens "github.com=$TOKEN"
